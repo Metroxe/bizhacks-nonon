a1759a55d13726df5cc85e747717cc4b27f05dd4
TaekHee Kwun <t.kwun@alumni.ubc.ca>
2018-03-24 03:21:55 -0700

    table filled

--- src/pages/data.js
+++ src/pages/data.js
@@ -13,16 +13,42 @@ module.exports = (app) => {
 			"</style></head>" +
 			"<h1>data</h1><br/>" +
 			"<button onclick='addSession()' type='button'>Add Session</button><input type=\"button\" value=\"Refresh Page\" onClick=\"window.location.reload()\">\n<br/><br/>" +
-			"<table style='width:100%'>";
+			"<table style=''>";
 
 		html += "<tr>" +
 			"<th>Customer Code</th>" +
-			"<th>Admin Code</th>";
+			"<th>Admin Code</th>" +
+			"<th>Customer Name</th>" +
+			"<th>Service</th>" +
+            "<th>Customer Address 1</th>" +
+            "<th>Customer Address 2</th>" +
+            "<th>City</th>" +
+            "<th>Province</th>" +
+            "<th>Postal Code</th>" +
+            "<th>Card #</th>" +
+            "<th>CVV</th>" +
+            "<th>Exp. MO</th>" +
+            "<th>Exp. YR</th>" +
+            "<th>Rep. Name</th>" +
+            "<th>Date</th>" ;
 
 		for (let session of sessions) {
 			html += "<tr>" +
-				"<th>" + session.customerCode + "</th>" +
-				"<th>" + session.adminCode + "</th>" +
+				"<td>" + session.customerCode + "</td>" +
+				"<td>" + session.adminCode + "</td>" +
+                "<td>" + session.customerName + "</td>" +
+                "<td>" + session.serviceArr + "</td>" +
+                "<td>" + session.customer_address1 + "</td>" +
+                "<td>" + session.customer_address2 + "</td>" +
+                "<td>" + session.city + "</td>" +
+                "<td>" + session.province + "</td>" +
+                "<td>" + session.postalCode + "</td>" +
+                "<td>" + session.cardNo + "</td>" +
+                "<td>" + session.cvv + "</td>" +
+                "<td>" + session.expirationMonth + "</td>" +
+                "<td>" + session.expirationYear + "</td>" +
+                "<td>" + session.repName + "</td>" +
+                "<td>" + session.date + "</td>" +
 				"</tr>";
 		}
 		html += "</table>";
