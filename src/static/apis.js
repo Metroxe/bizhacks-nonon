8f7fd24bfd06ad4c63717590dfd08bd911993ee4
Christopher Powroznik <chrispow96@gmail.com>
2018-03-24 03:45:50 -0700

    added data

--- src/static/apis.js
+++ src/static/apis.js
@@ -1,1 +1,49 @@
-console.log("hey");
+const id = getParameterByName("id");
+getSession();
+let customerCode;
+let adminCode;
+let customerName;
+let serviceArr;
+let status;
+let customer_address1;
+let customer_address2;
+let city;
+let province;
+let postalCode;
+let cardNo;
+let cvv;
+let expirationMonth;
+let expirationYear;
+let repName;
+let date;
+
+function getSession() {
+	axios.get("/get_session?id=" + id).then((res) => {
+		res.data.customerCode = customerCode;
+		res.data.adminCode = adminCode;
+		res.data.customerName = customerName;
+		res.data.serviceArr = serviceArr;
+		res.data.status = status;
+		res.data.customer_address1 = customer_address1;
+		res.data.customer_address2 = customer_address2;
+		res.data.city = city;
+		res.data.province = province;
+		res.data.postalCode = postalCode;
+		res.data.cardNo = cardNo;
+		res.data.cvv = cvv;
+		res.data.expirationMonth = expirationMonth;
+		res.data.expirationYear = expirationYear;
+		res.data.repName = repName;
+		res.data.date = date;
+	});
+}
+
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name        = name.replace(/[\[\]]/g, "\\$&");
+	var regex   = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return "";
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
